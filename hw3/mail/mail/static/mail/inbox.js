6d4ff7105ee31e87d5e7fe86141c26903be7a4aa
bitbucket <bitbucketicin@gmail.com>
2021-05-30 03:36:55 +0300

    before mail show function

--- hw3/mail/mail/static/mail/inbox.js
+++ hw3/mail/mail/static/mail/inbox.js
@@ -30,7 +30,19 @@ function load_mailbox(mailbox) {
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  document.querySelector('#emails-view-title').innerHTML = mailbox.charAt(0).toUpperCase() + mailbox.slice(1);
+
+  document.querySelector('#emails-div').style.display = 'none';
+  fetch('/emails/' + mailbox)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+      // ... do something else with emails ...
+
+      fillEmailsView(emails)
+      document.querySelector('#emails-div').style.display = 'flex';
+  });
 }
 
 function sendMail()
@@ -78,6 +90,49 @@ function alertnewMailForm(message, succes)
     document.querySelector('#compose-errmsg').classList.replace("alert-danger", "alert-success")
   else
     document.querySelector('#compose-errmsg').classList.replace("alert-success", "alert-danger")
+}
 
 
+function fillEmailsView(emails)
+{
+    document.querySelector('#emails-list').innerHTML = "";
+    document.querySelector('#emails-show').innerHTML = "";
+
+    if(emails.length === 0)
+    {
+      document.querySelector('#emails-emptytext').style.display = "block";
+      return;
+    }
+    
+    document.querySelector('#emails-emptytext').style.display = "none";
+    
+    // fill emails list
+    let mailListUL = document.createElement('ul')
+    mailListUL.classList.add("list-group", 'list-group-flush')
+
+    emails.forEach(email => {
+      let li = document.createElement("li")
+      li.classList.add("list-group-item")
+      
+      li.onclick = onMailClicked;
+      li.innerHTML = email.subject === "" ? "(No subject) from " + email.sender : email.subject;
+      li.dataset.mid = email.id;
+
+      mailListUL.appendChild(li)
+    });
+    
+    document.querySelector('#emails-list').appendChild(mailListUL)
+    
+    // make the selected mail 
+}
+
+function onMailClicked()
+{
+  
+
+  fetch('/emails/' + this.dataset.mid)
+    .then(response => response.json())
+    .then(email => {
+        document.querySelector('#emails-show').innerHTML = JSON.stringify(email)
+  });
 }
